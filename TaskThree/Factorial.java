1f1b2403b718dba2a50388e18d7a89bf5559cbb6
cheter <cheter2010@gmail.com>
2016-06-08 13:52:11 +0300

    Add files via upload

---
+++ TaskThree/Factorial.java
@@ -0,0 +1,34 @@
+package Task.TaskThree;
+
+/**
+ * Created by Cheter on 07.06.2016.
+ */
+import java.math.BigInteger;
+public class Factorial {
+    public static void main(String[] args)
+    {
+        /*for processing large numbers*/
+        BigInteger fact;
+        /* factorial of 1! is 1*/
+        fact= new BigInteger("1");
+        /* Loop to calculate the factorial of 100!*/
+        for (int i = 1; i <= 100; i++)
+        {
+            fact = fact.multiply(BigInteger.valueOf(i));// multiply all the numbers in order
+        }
+        System.out.print("100! = ");
+        System.out.println(fact);
+        System.out.print("sum of all digits in answer: ");
+        System.out.println(sumDigits(fact.toString()));
+    }
+    /* sum of elements in a row*/
+    private static int sumDigits(String s){
+        int sum = 0;
+        for (int i = 0; i < s.length(); i++)
+        {
+            int j = Integer.parseInt(s.substring(i,i+1));// Select an element
+            sum=sum+j;// sum the current item (the amount) with the following
+        }
+        return sum;
+    }
+}
