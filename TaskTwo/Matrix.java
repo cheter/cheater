e4d5cfcca52232529d3b5bdf5fd074e93b799edd
cheter <cheter2010@gmail.com>
2016-06-08 13:59:48 +0300

    Update Matrix.java

--- TaskTwo/Matrix.java
+++ TaskTwo/Matrix.java
@@ -4,18 +4,18 @@ package Task.TaskTwo;
  * Created by Cheter on 07.06.2016.
  */
 class Matrix {
-    public int[][] Cost;// An array that contains the value of the ribs(массив содержит значение цены)
+    public int[][] Cost;// An array that contains the value of the ribs
 
-    /* Argument is the number of vertices in the graph (номер вертикалей в матрице)*/
+    /* Argument is the number of vertices in the graph */
     public Matrix(int vertices){
 
         Cost = new int [vertices][vertices];
     }
-    /* Set the weight between the neighbors (утановка цены между соседями)*/
+    /* Set the weight between the neighbors */
     public void setEdge (int i, int j, int weight){
         Cost [i][j] = weight;
     }
-    /* Returns 0 if I and J are the same, a large number of returns if there is no connection between the tops of the ribs or weight(возвращаем 0 если I i J одинаковые, большее число возвр если нет соеденений между вершинами)) */
+    /* Returns 0 if I and J are the same, a large number of returns if there is no connection between the tops of the ribs or weight*/
     public int getCost(int i , int j){
 /* [0, 1, 3, 0]
  * [1, 0, 1, 4]
@@ -31,7 +31,7 @@ class Matrix {
     }
 
     /*Return the index of the smallest element of distances,
-      ignoring those in visited.(возвращаем индекс мин.дистации эл-та , игнорируем те которые уже были)*/
+      ignoring those in visited.*/
     protected int cheapest (Integer[] result, boolean [] visited){
         int best =-1;
         for (int i=0; i<Cost.length; i++){
@@ -42,17 +42,17 @@ class Matrix {
         return best;
     }
 
-    /*Return an array of the distances from source to each other vertex(возвращает массив растояний между источником и каждой вершиной)*/
+    /*Return an array of the distances from source to each other vertex*/
     public Integer[] distancesFrom (int source){
         Integer[] result = new Integer[Cost.length];
-        java.util.Arrays.fill(result,10001);// Set tops mark(устанавливаем наибольшую отметку)
-        result [source]=source;//Assign 1st top mark equal to "source"(присваевает первую найбольшую отметку равную "переменной(источнику)"
-        boolean []visited = new boolean [Cost.length];// visit the city(посещение города)
+        java.util.Arrays.fill(result,10001);// Set tops mark
+        result [source]=source;//Assign 1st top mark equal to "source"
+        boolean []visited = new boolean [Cost.length];// visit the city
         for (int i =0; i<Cost.length;i++){
-            int City = cheapest (result,visited);// Select the top of which has a minimum mark(определение вершины с мин. значением)
-            visited [City]=true;// Marking visited vertex(отмечаем посещенную вершину(город))
+            int City = cheapest (result,visited);// Select the top of which has a minimum mark
+            visited [City]=true;// Marking visited vertex
             for (int j =0; j<Cost.length;j++){
-                result [j] = Math.min(result[j], result[City]+getCost(City,j));// Write the smallest cost from one vertex to another(записываем наименьшую цену от 1-й вершине к другой)
+                result [j] = Math.min(result[j], result[City]+getCost(City,j));// Write the smallest cost from one vertex to another
             }
         }
         return result;
